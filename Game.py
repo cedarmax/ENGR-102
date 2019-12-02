d9279ea098e54db467cbfc5c035572f8c54bce3f
cedarmax <58445398+cedarmax@users.noreply.github.com>
2019-12-02 15:08:29 -0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -2,13 +2,13 @@
 #  “Aggies do not lie, cheat, or steal, or tolerate those who do”
 #  “I have not given or received any unauthorized aid on this assignment”
 # 
-# Names: 		Erwin Luevano
+# Names: 	Erwin Luevano
 #               Cedar Maxwell
 #               Alex Torres
 #               Muhammad Amer 
-# Section:		ENGR 102-537
-# Assignment:	Game project
-# Date:		    01 12 2019
+# Section:	ENGR 102-537
+# Assignment:	Battleship
+# Date:	  	11 December 2019
 
 import random as r
 def board():
@@ -27,80 +27,65 @@ def board():
 	yours = opponents[:]
 	for i in opponents: #prints out layout of chessboard before every move
 		print(*i, sep ='   ')
-	print('player 1')
+	print('\nplayer 1')
 	for i in yours:
 		print(*i, sep = '   ')		
 rows = 8
 columns = 8
 board()
 
+def rules():
+	'''This function will be run every time a move is made, checking if the move is valid'''
 
+def CPUBoardCreation():
+	'''This function will create the board if the player chooses to play the computer.'''
+	
+def PlayerBoardCreation():
+	'''This function will prompt the player to place his five pieces.'''
+	Carrier()
+	Battleship()
+	Cruiser()
+	Submarine()
+	Destroyer()
+	
+def Carrier():
+	'''Creates position for carrier (size = 5)'''
+	
+def Battleship():
+	'''Creates position for battleship (size = 4)'''
+	
+def Cruiser():
+	'''Creates position for cruiser (size = 3)'''
+	
+def Submarine():
+	'''Creates position for submarine (size = 3)'''
+def Destroyer():
+	'''Creates position for destroyer (size = 2)'''
 
+#The following seems to be the accepted method for setting up a program to move through a matrix.
+'''
+while True:
+    print('This is the Chessboard')
+    for i in range (rows):
+        for j in range (columns):
+            print(board[i][j], end='')
+        print('') #Required in order to print in columns.
 
-
-
-
-# =============================================================================
-# #The following seems to be the accepted method for setting up a program to move through a matrix.
-# 
-# while True:
-#     print('This is the Chessboard')
-#     for i in range (rows):
-#         for j in range (columns):
-#             print(board[i][j], end='')
-#         print('') #Required in order to print in columns.
-# 
-#     print('Where is your piece now?')
-#     row = int(input('Row?'))
-#     column = int(input('Column?'))
-#     row -= 1  #An adustment needed to be made since the row is technically from 0 to 7.
-#     column -= 1 #An adustment needed to be made since the column is technically from 0 to 7.
-#     if board [row][column] == '.':  #This was required to terminate the program if user accidentially asked to move from a blank space.
-#         print('You asked to move from a position where there was no piece, so the game has terminated.')
-#         break
-#     else:
-#         position = board[row][column]
-#         board[row][column] = '.'  #This is to replace the position with a dot.
-#         print('Where are you moving your piece?')
-#         row = int(input('Row?'))
-#         column = int(input('Column?'))
-#         row -= 1 #Again, an adjustment needed to be made since the row list is actually from 0 to 7.
-#         column -= 1 #An adustment needed to be made since the column is technically from 0 to 7.
-#         board[row][column] = position
-# 
-# 
-# =============================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print('Where is your piece now?')
+    row = int(input('Row?'))
+    column = int(input('Column?'))
+    row -= 1  #An adustment needed to be made since the row is technically from 0 to 7.
+    column -= 1 #An adustment needed to be made since the column is technically from 0 to 7.
+    if board [row][column] == '.':  #This was required to terminate the program if user accidentially asked to move from a blank space.
+        print('You asked to move from a position where there was no piece, so the game has terminated.')
+        break
+    else:
+        position = board[row][column]
+        board[row][column] = '.'  #This is to replace the position with a dot.
+        print('Where are you moving your piece?')
+        row = int(input('Row?'))
+        column = int(input('Column?'))
+        row -= 1 #Again, an adjustment needed to be made since the row list is actually from 0 to 7.
+        column -= 1 #An adustment needed to be made since the column is technically from 0 to 7.
+        board[row][column] = position
+'''
